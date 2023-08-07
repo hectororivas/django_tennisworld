9599875b38349b462d348bad9036c04d3be07f4e
horivas01 <horivas@gmail.com>
2023-08-07 13:31:33 -0400

    The project configuration was changed so that the database was migrated from sqlite to PostgreSQL V15

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tennisworld',
+        'USER': 'postgres',
+        'PASSWORD': 'xm34j51',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
